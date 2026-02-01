080d54d608fcc32f8adfdefc7abda94da372b133
Sachin Kumaram <sachin.kumaram@veoci.com>
2026-02-01 18:15:16 +0530

    Import Foundation framework in APPPrinterUnit.h

--- src/ios/APPPrinterUnit.h
+++ src/ios/APPPrinterUnit.h
@@ -18,7 +18,7 @@
  specific language governing permissions and limitations
  under the License.
  */
-
+@import Foundation;
 @interface APPPrinterUnit : NSObject
 
 + (double) convert:(nullable id)unit;
